e82fdebec6329281c3cd74c696b2edc87093d0ac
leohearts <leohearts@leohearts.com>
2021-02-17 13:38:24 +0800

    change font

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
     <title><?php $this->options->title(); ?><?php $this->archiveTitle(); ?></title>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;1,200;1,300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" media="all" href="<?php $this->options->themeUrl('css/main.css'); ?>" />
 </head>
 
