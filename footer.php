f75303e827f2494699f385fd685dd0e8dbefc80c
leohearts <leohearts@leohearts.com>
2021-02-19 15:59:04 +0800

    bug fix

--- footer.php
+++ footer.php
@@ -1,3 +1,13 @@
-<?php if (!empty($this->options->function) && in_array('LinkinNewtab', $this->options->function)) {
-    echo '<script>(function(){var res = document.querySelectorAll("a");for (var i=0;i<res.length;i++){  if(res[i].href!=""&&res[i].href.search(document.querySelectorAll("a")[2].href)){    res[i].target="_blank";res[i].rel="noopener";  }}})();</script>';
-}
+<?php if (!empty($this->options->function) && in_array('LinkinNewtab', $this->options->function)) : ?>
+    <script>
+        (function() {
+            [].forEach.call(document.querySelectorAll("a"), e => {
+                if (e.href != "" && new URL(e.href).hostname != location.host ){
+                    e.target = "_blank";
+                    e.rel = "noopener";
+                }
+            });
+        })();
+    </script>
+
+<?php endif; ?>
