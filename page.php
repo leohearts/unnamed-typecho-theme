df1a07231d38f4e09584a91639d913f9a0d1454f
leohearts <leohearts@leohearts.com>
2021-03-19 16:44:23 +0800

    change page style

--- page.php
+++ page.php
@@ -2,19 +2,19 @@
 
 <div class="main">
     <?php while ($this->next()) : ?>
-        <div class="post">
-            <div class="texts">
+        <div class="postPost">
+            <div class="postText">
                 <h1 class="entryTitle"><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h2>
-                <div class="entryTags">
-                    <p><?php $this->category('/'); ?> on <?php $this->date('Y/m/d'); ?></p>
-                </div>
-                <div class="entryText">
-                    <?php $this->content(''); ?>
-                </div>
+                    <div class="entryTags">
+                        <p><?php $this->category('/'); ?> on <?php $this->date('Y/m/d'); ?></p>
+                    </div>
+                    <div class="entryText">
+                        <?php $this->content(''); ?>
+                    </div>
             </div>
         </div>
     <?php endwhile; ?>
-    
+
 </div>
 
 <?php $this->need("footer.php"); ?>
