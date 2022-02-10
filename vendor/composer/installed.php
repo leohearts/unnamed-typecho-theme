7d9274f3b5e33f292643d006e3b4e405f36b9c14
Leohearts Taro <leohearts@leohearts.com>
2022-02-11 00:04:28 +0800

    add blurhash

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e18f1d54a1b440ccd775542b0ec69fd42dee5ae3',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e18f1d54a1b440ccd775542b0ec69fd42dee5ae3',
+            'dev_requirement' => false,
+        ),
+        'kornrunner/blurhash' => array(
+            'pretty_version' => 'v1.2.1',
+            'version' => '1.2.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../kornrunner/blurhash',
+            'aliases' => array(),
+            'reference' => '7dde9c0c7ee7e688dc79fb7770ee110ed308ffc7',
+            'dev_requirement' => false,
+        ),
+    ),
+);
