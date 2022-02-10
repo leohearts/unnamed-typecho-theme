ef070a73d643cbec7e21c45acc5ddb6dcb2ab13a
Leohearts Taro <leohearts@leohearts.com>
2022-02-11 04:03:21 +0800

    fix Uncaught TypeError: document.getElementsByClassName(...)[0] is undefined

--- js/post.js
+++ js/post.js
@@ -1,9 +1,13 @@
-[].forEach.call(document.getElementsByClassName("postPost")[0].querySelectorAll("img"), e => {
-    var newImageItem = e.cloneNode();
-    var newItem = document.createElement('a');
-    newItem.href = e.src;
-    newItem.setAttribute('data-fslightbox', 'gallery');
-    newImageItem.loading = "lazy";
-    newItem.append(newImageItem);
-    e.parentNode.replaceWith(newItem);
-})
+window.onload = ()=>{
+    try{
+        [].forEach.call(document.getElementsByClassName("postPost")[0].querySelectorAll("img"), e => {
+            var newImageItem = e.cloneNode();
+            var newItem = document.createElement('a');
+            newItem.href = e.src;
+            newItem.setAttribute('data-fslightbox', 'gallery');
+            newImageItem.loading = "lazy";
+            newItem.append(newImageItem);
+            e.parentNode.replaceWith(newItem);
+        })
+    }
+}
