984a7c727890b9ed02a2db618b9a7ffa74bb6715
Leohearts Taro <leohearts@leohearts.com>
2022-09-11 19:37:24 +0700

    add about page

---
+++ about.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * 自定义页面模板
+ *
+ * @package custom
+ */
+
+$this->need('header.php'); ?>
+
+<div class="main">
+    <?php while ($this->next()) : ?>
+        <div class="postPost">
+            <div class="postText">
+                    <div class="entryText">
+                        <?php $this->content(''); ?>
+                    </div>
+            </div>
+        </div>
+    <?php endwhile; ?>
+
+</div>
+
+<?php $this->need("footer.php"); ?>
