df717d719366f72aa416d4d965b76023bdd8bdfa
Leohearts <leohearts@leohearts.com>
2023-12-12 17:32:25 +0800

    feat: rollback to markdown

--- frontpage.php
+++ frontpage.php
@@ -7,21 +7,20 @@
 
 $this->need('header.php'); ?>
 
+
 <div class="main">
+    <?php while ($this->next()) : ?>
         <div class="postPost">
             <div class="postText">
-                <div class="entryText">
-                    <h1>🥔 Hi there, I'm Leohearts!</h1>
-                    <p>可以叫我芋头～</p>
-                    <p>这里是我的页面，用来记录一些自己遇到的有趣的事情，以及一点点的心中所想。<br><del>但可能更像一个技术博客？</del></p>
-                    <h2>欢迎！</h2>
-                    <a href="./blog.html">博客</a>
-                    <a href="./friends.html">友链</a>
-                    <a href="https://t.me/whatdoespotatoeattoday">芋头今天吃什么</a>
-                </div>
+                    <div class="entryText">
+                        <?php $this->content(''); ?>
+
+                    </div>
             </div>
         </div>
+    <?php endwhile; ?>
 </div>
+
 
 
 <?php $this->need("footer.php"); ?>
