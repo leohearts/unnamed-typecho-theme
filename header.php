083a6e8d3f37c18f9d2f118d7ab8b5190379b7f8
Leohearts <leohearts@leohearts.com>
2024-02-07 01:52:29 +0800

    fix ^

--- header.php
+++ header.php
@@ -31,7 +31,7 @@
     <script src="<?php $this->options->themeUrl('js/prism.js'); ?>" async></script>
     <?php if ($this->is('post')) : ?>
         <script src="<?php $this->options->themeUrl('js/post.js'); ?>" async></script>
-        <?php if(isset($this->fields->fslightbox) && $this->fields->fslightbox == "no") : ?>
+        <?php if(!(isset($this->fields->fslightbox) && $this->fields->fslightbox == "no")) : ?>
         <script src="https://cdn.jsdelivr.net/npm/fslightbox@3.3.1/index.js" async defer></script>
         <?php endif; ?>
     <?php endif; ?>
