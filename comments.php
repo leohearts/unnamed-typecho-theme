b33d5251640d2e9f066b0a42097bc08a2a029e87
Leohearts <leohearts@leohearts.com>
2024-07-19 14:24:53 +0800

    📝 (comments.php): add CSS styles for comment form layout and improve responsiveness 🔧 (comments.php): fix missing closing brackets in HTML for input-id class elements

--- comments.php
+++ comments.php
@@ -1,5 +1,36 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <div id="comments">
+    <style>
+.comment-form {
+  display: flex;
+}
+
+#response {
+  width: 100%;
+  display: block;
+}
+
+.comment-content label {
+  display: none;
+}
+
+.input-id {
+  display: flex;
+  flex-direction: column;
+  width: 30%;
+  align-self: center;
+  margin-right: 20px;
+}
+
+#comment-form {
+  display: flex;
+  flex-direction: column;
+}
+
+.input-id-box {
+  display: flex;
+}
+    </style>
     <?php $this->comments()->to($comments); ?>
     <?php if ($comments->have()): ?>
         <h3><?php $this->commentsNum(_t('暂无评论'), _t('仅有一条评论'), _t('已有 %d 条评论')); ?></h3>
@@ -19,8 +50,7 @@
             <h3 id="response"><?php _e('添加新评论'); ?></h3>
             <form method="post" action="<?php $this->commentUrl() ?>" id="comment-form" role="form">
                 <p class="comment-content">
-                    <label for="textarea" class="required comment-content"><?php _e('内容'); ?></label>
-                    <textarea rows="8" cols="50" name="text" id="textarea" class="textarea"
+                    <textarea rows="8" cols="50" name="text" placeholder="写点什么吧" id="textarea" class="textarea"
                               required><?php $this->remember('text'); ?></textarea>
                 </p>
                 <div class="input-id-box">
@@ -30,18 +60,18 @@
                             href="<?php $this->options->logoutUrl(); ?>" title="Logout"><?php _e('退出'); ?> &raquo;</a>
                     </p>
                 <?php else: ?>
-                    <p class="input-id>
+                    <p class="input-id">
                         <label for="author" class="required"><?php _e('称呼'); ?></label>
                         <input type="text" name="author" id="author" class="text"
                                value="<?php $this->remember('author'); ?>" required/>
                     </p>
-                    <p class="input-id>
+                    <p class="input-id">
                         <label
                             for="mail"<?php if ($this->options->commentsRequireMail): ?> class="required"<?php endif; ?>><?php _e('Email'); ?></label>
                         <input type="email" name="mail" id="mail" class="text"
                                value="<?php $this->remember('mail'); ?>"<?php if ($this->options->commentsRequireMail): ?> required<?php endif; ?> />
                     </p>
-                    <p class="input-id>
+                    <p class="input-id">
                         <label
                             for="url"<?php if ($this->options->commentsRequireURL): ?> class="required"<?php endif; ?>><?php _e('网站'); ?></label>
                         <input type="url" name="url" id="url" class="text" placeholder="<?php _e('http://'); ?>"
