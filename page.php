16976a71f9527bf3b3a866ffcf7527df606e683d
Leohearts <leohearts@leohearts.com>
2024-11-16 16:30:33 +0800

    💄 style(blog): update entry tags and date format
    - add emoji to tags and date for better readability
    - remove unnecessary closing div tag in page.php
    - add views and likes count to page.php for engagement metrics

--- page.php
+++ page.php
@@ -6,8 +6,7 @@
             <div class="postText">
                 <h1 class="entryTitle"><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h1>
                     <div class="entryTags">
-                        <p><?php $this->category('/'); ?> <?php $this->tags('/', true, '') ?> on <?php $this->date('Y/m/d'); ?></p>
-                    </div>
+                        <p class="get-likes">🏷️<?php $this->category('/'); ?>/<?php $this->tags('/', true, '') ?> 🕒<?php $this->date('Y/m/d'); ?> 👀<?php $this->viewsNum(); ?> <i class="set-likes iconfont icon-zan" data-cid="<?php $this->cid(); ?>">💖</i><?php $this->likesNum(); ?></p>                    </div>
                     <div class="entryText">
                         <?php $this->content(''); ?>
                     </div>
