88e1e85b6186edb450dcbeee68dfee2d6eb97153
Leohearts <leohearts@leohearts.com>
2024-11-17 20:07:45 +0800

    feat: add friend description and add time

--- js/friends.js
+++ js/friends.js
@@ -3,7 +3,13 @@ friendsData = [
     {
         "name": "zeocax",
         "image": "http://qwq",
-        "href": "qwq"
+        "href": "qwq",
+        "description": "一只爱折腾的绯鞠，是非常高产又有爱的博主",
+        "style": {
+            "fontFamily": "Borel"
+        },
+        "join": "2024年的秋天"
+
     }
 ]
 */
@@ -16,7 +22,16 @@ window.onload = (() => {
         let friendImage = document.createElement("img");
         friendImage.src = e.image;
         friendImage.alt = e.name;
+        let friendData = document.createElement("div");
+        friendData.className = "friendData";
+
+        let friendJoin = document.createElement("span");
+        friendJoin.className = "friendJoin";
+        friendJoin.innerText = e.join;
+        friendData.appendChild(friendJoin);
+
         let friendName = document.createElement("p");
+        friendName.className = "friendName";
         friendName.innerText = e.name;
         if (e.style != undefined) {
             if ("fontFamily" in e.style) {
@@ -29,11 +44,19 @@ window.onload = (() => {
                 friendName.style[entry] = e.style[entry];
             }
         }
+        friendData.appendChild(friendName);
+
+        let friendDesc = document.createElement("span");
+        friendDesc.className = "friendDesc";
+        friendDesc.innerText = e.description;
+        friendData.appendChild(friendDesc);
+
         if (e.broken == true) {
             friendEntry.classList.add("broken");
         }
         friendEntry.appendChild(friendImage);
-        friendEntry.appendChild(friendName);
+
+        friendEntry.appendChild(friendData);
         friendsRoot.appendChild(friendEntry);
     })
 })
