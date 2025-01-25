815bb0dbf24665a0214e440abec49844777c3af8
Leohearts <leohearts@leohearts.com>
2025-01-25 13:48:12 +0800

    update divider style

--- license.php
+++ license.php
@@ -1,5 +1,5 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
-<div class="divider">
+<div class="divider" style="margin: 2em 10%;">
 </div>
 <blockquote id="license" style="display:flex;width: 90%;justify-content: space-between;margin-top: 1em;margin-bottom: 1.5em;">
  <p xmlns:cc="http://creativecommons.org/ns#" style="/*! float: left; */">This work is licensed under<br><a href="https://creativecommons.org/licenses/by-nc/4.0/" target="_blank" rel="license noopener noreferrer">Creative Commons Attribution-NonCommercial 4.0 International</a></p>
